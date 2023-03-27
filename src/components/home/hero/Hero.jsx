a61498e5c55590a84d6e7efd156e09c946079ff1
hussainna <hissenabd5@gmail.com>
2023-03-27 19:40:31 +0100

    add the front end files

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,14 +1,60 @@
 import React from 'react'
 import './hero.scss'
+import { Link, useHistory } from 'react-router-dom'
+import axios from 'axios'
+import swal from 'sweetalert'
+
 import {FaPizzaSlice} from 'react-icons/fa'
 function Hero() {
+
+    const history=useHistory()
+
+    var authButton='';
+
+    const Logout=(e)=>{
+        e.preventDefault()
+
+        axios.post('logout').then(res=>{
+          if(res.data.status===200)
+          {
+            localStorage.removeItem('auth_token')
+            localStorage.removeItem('auth_user')
+            swal('success',res.data.status);
+            history.push('/')
+          }
+        })
+    }
+    
+  if(!localStorage.getItem('auth_token'))
+  {
+    authButton=(
+      <ul className='flex'>
+      <li><Link to='/login'>Login</Link></li>
+      <li><Link to='/register'>Register</Link></li>
+
+    </ul>
+    )
+  }
+  else{
+    authButton=(
+      <button onClick={Logout}>Logout</button>
+      
+
+    )
+  }
+
   return (
     <div className='hero'>
         <div className="container">
             <div className="top">
                 <div className="row">
-                    <div className="div"></div>
+                    <div className="div">
+                    {authButton}
+                    </div>
+                    <div className="center">
                     <h2>Pizza</h2>
+                    
+                    </div>
                     <h4>Menu
                         <i><FaPizzaSlice/></i>
                     </h4>
